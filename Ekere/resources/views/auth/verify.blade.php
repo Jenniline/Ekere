31b16b6a47a71fca91d8cd2dd4ee6d5c4ad60826
Jenniline <jennilineebai@gmail.com>
2021-07-08 13:35:16 +0100

    Added image to verify email page

--- Ekere/resources/views/auth/verify.blade.php
+++ Ekere/resources/views/auth/verify.blade.php
@@ -14,14 +14,16 @@
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
+                    <img src=" {{asset('img/verify-email.png')}} " alt="" class="img-d img-fluid" style="width:600px; height:400px"> 
+                    <h1 class="text-success">Verify your email address</h1>
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                   <h3>  {{ __('Before proceeding, please check your email for a verification link.') }}</h3>  
+                    <h4> {{ __('If you did not receive the email') }}, </h4>
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
